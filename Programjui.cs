392eda8b7713fcc67e34ea4c03d997c7146d1fd2
Al3x08bachka <157345073+Al3x08bachka@users.noreply.github.com>
2024-01-24 08:27:46 +0200

    Add files via upload

---
+++ Programjui.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        newmaybe();
+
+        static void newmaybe()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("kak sa kazvash ve");
+        name: Console.ReadLine();
+            Console.WriteLine("Priqtno mi e");
+
+        }
+    }
+}
